54d635578be3ec565a1471d2b09754a216ef02ae
William Balea <william.balea@polymtl.ca>
2018-11-15 23:23:04 -0500

    tout ou rien

--- Top.vhd
+++ Top.vhd
@@ -8,7 +8,12 @@ entity Top is
     CLK : in std_logic;
     reset_top : in std_logic;
     uart_in : in std_logic;
-    erreur_top : out std_logic
+    erreur_top : out std_logic;
+    
+    UART_RXD_OUT : out std_logic;
+    an : out std_logic_vector( 7 downto 0);
+    ca, cb, cc, cd, ce, cf, cg, dp : out std_logic
+    
     );
 end Top;
 
@@ -34,11 +39,23 @@ Port(
 );
 end component StateMachine;
 
---signal resetDiviseur : std_logic;
---signal diviseurCLK : std_logic;
+component aff7seg is
+port (
+        clk100mhz : in std_logic;
+        heures_1,   heures_2    : in std_logic_vector(7 downto 0);
+        minutes_1,  minutes_2   : in std_logic_vector(7 downto 0);
+        secondes_1, secondes_2  : in std_logic_vector(7 downto 0);
+        centiemes_1,centiemes_2 : in std_logic_vector(7 downto 0);
+        an : out std_logic_vector(7 downto 0);
+        ca, cb, cc, cd, ce, cf, cg, dp : out std_logic
+    );
+end component aff7seg;
+
+
 signal donnee_recu : std_logic_vector (7 downto 0);
 signal diviseurCLK1 : std_logic;
 signal diviseurCLK2 : std_logic;
+signal sa, sb, sc, sd, se, sf, sg, sp : std_logic_vector (7 downto 0) := "0000000";
 
 begin
 
@@ -62,5 +79,37 @@ Port map (
     --diviseurReset => resetDiviseur,
     donnee => donnee_recu
     );
+UUT3 : aff7seg
+port map (
+        clk100mhz => CLK,
+        heures_1    => sg, 
+        heures_2    => sp,
+        minutes_1   => se, 
+        minutes_2   => sf,
+        secondes_1  => sc, 
+        secondes_2  => sd,
+        centiemes_1 => sa, 
+        centiemes_2 => sb,
+        an => an,
+        ca => ca, 
+        cb => cb, 
+        cc => cc, 
+        cd => cd,
+        ce => ce, 
+        cf => cf, 
+        cg => cg, 
+        dp => dp
+    );
+    
+sa <= sa & donnee_recu(0);
+sb <= sb & donnee_recu(1);
+sc <= sc & donnee_recu(2);
+sd <= sd & donnee_recu(3);
+se <= se& donnee_recu(4);
+sf <= sf & donnee_recu(5);
+sg <= sg & donnee_recu(6);
+sp <= sp & donnee_recu(7);
+
+UART_RXD_OUT <= uart_in;
 
 end Behavioral;
